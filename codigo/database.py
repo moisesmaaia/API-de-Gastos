eb5b516284ca79112761f0d17a8110311dbd8567
moisesmaaia <samueelmaia28@gmail.com>
2026-05-05 22:48:46 -0300

    fix: corrigir carregamento do .env e configuração do db

--- codigo/database.py
+++ codigo/database.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "mysql+pymysql://api_user:123456@localhost/api_gastos"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
